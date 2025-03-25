d63b77841e186a235f955e92fb7d26316d989504
den232i <id12-97@mail.ru>
2025-03-25 19:13:33 +0300

    impl preparing of database url

--- app/settings.py
+++ app/settings.py
@@ -3,9 +3,12 @@ from envparse import Env
 
 env = Env()
 
-DATABASE_URL = env.str(
-    "DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
-)
-URL_PREFIX = "/api/v1"
+# Postgres
+POSTGRES_SERVER = env.str("POSTGRES_SERVER", default="localhost")
+POSTGRES_PORT = env.str("POSTGRES_PORT", default="5432")
+POSTGRES_DB = env.str("POSTGRES_DB", default="postgres")
+POSTGRES_USER = env.str("POSTGRES_USER", default="postgres")
+POSTGRES_PASSWORD = env.str("POSTGRES_PASSWORD", default="postgres")
 
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+URL_PREFIX = "/api/v1"
